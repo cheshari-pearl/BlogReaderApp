0afd49fef50177810fd45c8497d3b9955832027f
cheshari-pearl <chesharipearl@gmail.com>
2024-06-25 16:06:51 +0300

    first commit

---
+++ app/src/main/java/com/akirachix/blogreader/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.akirachix.blogreader
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.akirachix.blogreader.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/com/akirachix/blogreader/articles.kt
@@ -0,0 +1,13 @@
+package com.akirachix.blogreader
+
+
+data class Article(
+    val author: String,
+    val authorProfilePicture: String,
+    val title: String,
+    val publishingDate: String,
+    val articlePreview: String,
+    val articlePhoto: String,
+    val fullPostLink: String
+)
+
