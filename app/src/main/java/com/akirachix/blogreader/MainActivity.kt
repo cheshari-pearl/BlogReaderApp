aa68f2ed303f4076edf5419a565d858599c02e51
cheshari-pearl <chesharipearl@gmail.com>
2024-06-26 07:38:02 +0300

    first commit

--- app/src/main/java/com/akirachix/blogreader/MainActivity.kt
+++ app/src/main/java/com/akirachix/blogreader/MainActivity.kt
@@ -15,5 +15,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        displayAuthors()
+    }
+
+    fun displayAuthors(){
+        val article1 = Article("Paulo Coelho","","Veronica Decides To Die","","2010","Hanif Abdurraqib's latest book is a transcendent feat of poetry, memoir, and—well, magic. His narration is as breathless and beautiful as his prose. It's an ode to his hometown of Columbus, Ohio; a love letter to basketball; a meditation on home and belonging; and an exploration of faith, Blackness, music, and place.","Read more")
+        val article2 = Article("House Rules","","Jodi Picoult","","1990","In order to cultivate relationships that increase their power, people seeking power should force others to come to them, never allow others to become independent of them, avoid anyone who frequently encounters misfortune, never place too much trust in any one person, and never outperform their superiors.","Read more")
+        val article3 = Article("B.A Paris","","Robert Greene","","1990","In order to cultivate relationships that increase their power, people seeking power should force others to come to them, never allow others to become independent of them, avoid anyone who frequently encounters misfortune, never place too much trust in any one person, and never outperform their superiors.","Read more")
+
+        val articleList = listOf(article1,article2,article3)
+        val articleAdapter = ArticleAdapter(articleList)
+        binding.rvArticles.adapter = articleAdapter
     }
 }
--- app/src/main/java/com/akirachix/blogreader/article_adapter.kt
+++ app/src/main/java/com/akirachix/blogreader/article_adapter.kt
@@ -1,4 +1,40 @@
 package com.akirachix.blogreader
 
-class ArticleAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ArticleAdapter(val articleList: List<Article>):RecyclerView.Adapter<ArticleViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        var itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.article_list_item,parent, false)
+        return ArticleViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = articleList[position]
+        holder.tvAuthor.text=article.author
+        holder.tvLink.text = article.fullPostLink
+        holder.tvArticleTitle.text = article.title
+        holder.tvDate.text = article.publishingDate
+        holder.tvArticlesPreview.text = article.articlePreview
+
+
+    }
+
+
+}
+class ArticleViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    var tvAuthor:TextView = itemView.findViewById(R.id.tvAuthor)
+    var tvArticleTitle:TextView = itemView.findViewById(R.id.tvArticleTitle)
+    var tvDate:TextView = itemView.findViewById(R.id.tvDate)
+    var tvArticlesPreview:TextView = itemView.findViewById(R.id.tvArticlesPreview)
+    var tvLink:TextView = itemView.findViewById(R.id.tvLink)
 }
